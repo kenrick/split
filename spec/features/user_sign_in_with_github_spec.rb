1c94d946351b3b721780b6739b6858b85185cfda
Kenrick Beckett <kenrick.beckett@gmail.com>
2015-04-03 20:09:25 -0500

    Update sign in with github feature

--- spec/features/user_sign_in_with_github_spec.rb
+++ spec/features/user_sign_in_with_github_spec.rb
@@ -2,12 +2,30 @@ require 'rails_helper'
 
 RSpec.feature 'Signing in with github' do
 
-  scenario 'sign in successfully' do
-    visit '/'
+  background do
+    OmniAuth.config.add_mock(:github, uid: "12345", info: { name: 'john' })
+  end
 
+  scenario 'sign in for the first time' do
+    expect(User.count).to eq(0)
+
+    visit '/'
     click_link 'Sign in with Github'
 
     expect(page).to have_content('Signed in sucessfully')
+    expect(page).to have_content('Howdy john!')
+    expect(User.count).to eq(1)
+    expect(User.last.uid).to eq("12345")
+  end
+
+  scenario 'sign in as existing user' do
+    create :user
+
+    visit '/'
+    click_link 'Sign in with Github'
+
+    expect(page).to have_content('Signed in sucessfully')
+    expect(User.count).to eq(1)
   end
 end
 
